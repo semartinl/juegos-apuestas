eece8e24702d791cac6ade16f8f3597bba52d40a
semartinl <semartinl@alumnos.unex.es>
2024-07-29 17:58:44 +0200

    Añadido log in

--- client/src/Routes/LogIn.jsx
+++ client/src/Routes/LogIn.jsx
@@ -1,11 +1,13 @@
 import styleMain from './../css/main.module.css'
+import styleForm from './../css/form.module.css'
 export function LogIn(){
     return (
         <main className={styleMain["container-main"]}>
-            <form action="/login">
-                <input type="text" name="username" id="username"  />
-                <input type="password" name="password" id="password" />
-                <input type="submit" value="Iniciar sesión" />
+            <form action="/login" className={styleForm['main-form']}>
+            <h1>INICIAR SESIÓN</h1>
+                <input className='cicle-border' type="text" name="username" id="username"  />
+                <input className='cicle-border' type="password" name="password" id="password" />
+                <input className='cicle-border' type="submit" value="Iniciar sesión" />
             </form>
         </main>
     )
