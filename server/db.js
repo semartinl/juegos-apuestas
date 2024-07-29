c7343b76d5ac0eac55259614e9ca9b6498857cd6
rvcuban <78472187+rvcuban@users.noreply.github.com>
2024-07-29 18:31:19 +0200

    atlas mongo

--- server/db.js
+++ server/db.js
@@ -1,6 +1,7 @@
 import mongoose, { connect } from "mongoose";
 export const connectDB = async () => {
     try {
+        
         await mongoose.connect('mongodb+srv://jrodriguezverdecia:Is2AmmNwKBvwxFYX@clusterapuestas.ril26k3.mongodb.net/?retryWrites=true&w=majority&appName=ClusterApuestas');
         console.log("db is connected");
     } catch (error) {
