9559d27f7d440c55dc033e597ff405045fc7230e
semartinl <semartinl@alumnos.unex.es>
2024-08-07 08:37:02 +0200

    Casi terminado estilado y funcionalidades básicas de la página principal

--- client/src/Components/DescriptionGame.jsx
+++ client/src/Components/DescriptionGame.jsx
@@ -1,10 +1,13 @@
+import { useNavigate } from "react-router"
 import styleMain from "./../css/main.module.css"
 export function DescriptionGame({game}){
+    const navigate = useNavigate()
     return (
         <div className={styleMain["container-game"]}>
             <div className={styleMain["description-game"]}>
                 <h3><span className={styleMain["versus"]}>{game.numberVersus}</span> {game.title}</h3>
                 <p>{game.description}</p>
+                <button className={"secondary-color-button cicle-border"} onClick={()=>navigate(`/games/${game.id}`)}>Play now</button>
             </div>
             <img className={styleMain["img-game"] } src={game.image} alt={`Imagen del juego de ${game.title}`} />
         </div>
